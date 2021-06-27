efe1bbdb922caba0b80934ca2c41faf246e74d65
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 15:30:08 +0530

    Initial Commit

---
+++ src/com/linecompare/LineComparision.java
@@ -0,0 +1,10 @@
+package com.linecompare;
+
+public class LineComparision {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the Line Comparision Program");
+	}
+
+}
