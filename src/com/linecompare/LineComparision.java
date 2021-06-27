f3dbc0090618f8c4b26b871a149fa9c9d6b1c050
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 16:09:52 +0530

    ADDED:Comparing two lines using compare to method

--- src/com/linecompare/LineComparision.java
+++ src/com/linecompare/LineComparision.java
@@ -30,11 +30,14 @@ public class LineComparision
         Double length_of_line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
 
 		System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
-		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);			boolean ans = length_of_line1.equals(length_of_line2);
-		if(ans)
-				System.out.println("Both lines are equal.");
-		else
-				System.out.println("Both lines are not equal.");	
+		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);			
+		int res = length_of_line1.compareTo(length_of_line2);
+        if(res > 0)
+                System.out.println("Length of first end points is greater than length of second end points");
+        else if(res < 0)
+                System.out.println("Length of first end points is less than length of second end points");
+        else
+                System.out.println("Both lines are equal.");	
 	}
 }
 
