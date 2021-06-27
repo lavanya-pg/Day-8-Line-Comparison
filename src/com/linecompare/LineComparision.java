93425477ed539656a6f861b5b1d7b2974e8d7eb2
lavanya-pg <85681326+lavanya-pg@users.noreply.github.com>
2021-06-27 16:04:36 +0530

    Update LineComparision.java

--- src/com/linecompare/LineComparision.java
+++ src/com/linecompare/LineComparision.java
@@ -8,14 +8,14 @@ public class LineComparision
 	{
 		System.out.println("Welcome to the Line Comparision Program");
 		Scanner s = new Scanner(System.in);
-        System.out.println("Enter x co-ordinate of first point: ");
-        int x1 = s.nextInt();
-        System.out.println("Enter y co-ordinate of first point: ");
-        int y1 = s.nextInt();
-        System.out.println("Enter x co-ordinate of second point: ");
-        int x2 = s.nextInt();
-        System.out.println("Enter y co-ordinate of second point: ");
-        int y2 = s.nextInt();
+        	System.out.println("Enter x co-ordinate of first point: ");
+       		 int x1 = s.nextInt();
+        	System.out.println("Enter y co-ordinate of first point: ");
+        	int y1 = s.nextInt();
+        	System.out.println("Enter x co-ordinate of second point: ");
+        	int x2 = s.nextInt();
+        	System.out.println("Enter y co-ordinate of second point: ");
+        	int y2 = s.nextInt();
 
 		System.out.println("Enter x co-ordinate of third point: ");
 		int p1 = s.nextInt();
@@ -26,8 +26,8 @@ public class LineComparision
 		System.out.println("Enter y co-ordinate of forth point: ");
 		int q2 = s.nextInt();
 
-        Double length_of_line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-        Double length_of_line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+       	 	Double length_of_line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        	Double length_of_line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
 
 		System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
 		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
