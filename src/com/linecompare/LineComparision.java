b95190b3af04f6f39f5b32e4405b04841a3cdc5b
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 15:43:25 +0530

    ADDED:length of the end-points

--- src/com/linecompare/LineComparision.java
+++ src/com/linecompare/LineComparision.java
@@ -1,10 +1,27 @@
 package com.linecompare;
 
-public class LineComparision {
+import java.util.Scanner;
 
+public class LineComparision
+{
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to the Line Comparision Program");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter x co-ordinate of first point: ");
+		int x1 = s.nextInt();
+		System.out.println("Enter y co-ordinate of first point: ");
+		int y1 = s.nextInt();
+		System.out.println("Enter x co-ordinate of second point: ");
+		int x2 = s.nextInt();
+		System.out.println("Enter y co-ordinate of second point: ");
+		int y2 = s.nextInt();
+
+		double lenght_of_line = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.printf("Length of end point is : %.2f %n", lenght_of_line);
+		
+		}
+
 	}
 
-}
+
